60f9c1343a1085788f02f3c6129fb4e5803149f5
titovanton-com <mail@titovanton.com>
2013-08-02 18:05:08 +0600

    post mk venv fixes

--- templates/admin.py
+++
@@ -1,5 +0,0 @@
-# coding: UTF-8
-
-from django.contrib import admin
-from django import forms
-from sorl.thumbnail.admin import AdminImageMixin
--- templates/forms.py
+++
@@ -1,7 +0,0 @@
-# coding: UTF-8
-
-from django import forms
-# from captcha.fields import ReCaptchaField
-
-import messages
-
