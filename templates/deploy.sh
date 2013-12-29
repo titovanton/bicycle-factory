8626f8c4730c1d27bf263c09a38b3280762f3472
titovanton-com <mail@titovanton.com>
2013-12-29 10:16:20 +0600

    templates has beed added

---
+++ templates/deploy.sh
@@ -0,0 +1,7 @@
+sudo chown -R titovanton:www-data %DJANGO%
+sudo chmod -R 771 %DJANGO%
+sudo chmod -R 775 %STATIC%
+sudo chmod -R 775 %MEDIA%
+sudo chmod -R 775 %INTERNAL%
+sudo touch reload_uwsgi
+sudo service nginx --full-restart
