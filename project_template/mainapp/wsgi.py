e5489841d7eeecbf3042c12f27b12de204868b6e
titovanton <mail@titovanton.com>
2014-01-14 00:32:34 +0600

    .gitignore fixes

---
+++ project_template/mainapp/wsgi.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainapp.settings")
+application = get_wsgi_application()
