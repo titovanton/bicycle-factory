c132bde56689424d581b9699d79bc86e19061a90
titovanton <mail@titovanton.com>
2014-04-12 06:36:35 +0600

    deploy dummy

--- templates/deploy.sh
+++ templates/deploy.sh
@@ -11,9 +11,11 @@ chown -R $USR:www-data %PROJECT_DIR%
 chown -R $USR:www-data %STATIC%
 chown -R $USR:www-data %MEDIA%
 chown -R $USR:www-data %INTERNAL%
+chown -R $USR:www-data %DUMMY_ROOT%
 chmod -R 771 %PROJECT_DIR%
 chmod -R 775 %STATIC%
 chmod -R 775 %MEDIA%
 chmod -R 775 %INTERNAL%
+chmod -R 775 %DUMMY_ROOT%
 touch reload_uwsgi
 service nginx --full-restart
