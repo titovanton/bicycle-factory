7578cefade22fb803cf91351d3217201f0a7c87c
titovanton <mail@titovanton.com>
2014-07-12 09:01:02 +0600

    ES plugin fixes

--- templates/get_up_ubuntu.sh
+++ templates/get_up_ubuntu.sh
@@ -136,9 +136,7 @@ if $ELASTICSEARCH; then
     dpkg -i elasticsearch-1.1.1.deb
     /usr/share/elasticsearch/bin/plugin \
                 -install analysis-morphology \
-                -url http://dl.bintray.com/content/imotov/elasticsearch-plugins/\
-                org/elasticsearch/elasticsearch-analysis-morphology/1.2.0/\
-                elasticsearch-analysis-morphology-1.2.0.zip
+                -url http://dl.bintray.com/content/imotov/elasticsearch-plugins/org/elasticsearch/elasticsearch-analysis-morphology/1.2.0/elasticsearch-analysis-morphology-1.2.0.zip
     update-rc.d elasticsearch defaults 95 10
     sed -e "s/^# network\.host: .*$/network.host: 127.0.0.1/g" -i /etc/elasticsearch/elasticsearch.yml
 fi
